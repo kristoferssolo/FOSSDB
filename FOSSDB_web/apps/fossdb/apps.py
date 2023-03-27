6dc7acbd7ca0e5d99e3cd4c014b7c0f77baaf7a6
Kristofers Solo <kristians.cagulis@proton.me>
2023-03-27 09:23:17 +0300

    Changes `'` to `"`

--- FOSSDB_web/apps/fossdb/apps.py
+++ FOSSDB_web/apps/fossdb/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FossdbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fossdb'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fossdb"
