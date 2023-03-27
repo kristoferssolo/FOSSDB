5448d0e624dbda5f2ce1fb128cdec71ec36a643e
Kristofers Solo <kristians.cagulis@proton.me>
2023-03-27 20:26:19 +0300

    Set login/logout redirect

--- FOSSDB_web/settings.py
+++ FOSSDB_web/settings.py
@@ -133,3 +133,5 @@ MEDIA_ROOT = BASE_PATH.joinpath("media")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = ""
+LOGOUT_REDIRECT_URL = ""
