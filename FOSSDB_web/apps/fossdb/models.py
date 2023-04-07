341ce260b9adbe1ab0af6405a1de18bbc0b735e6
Kristofers Solo <kristians.cagulis@proton.me>
2023-04-07 14:29:38 +0300

    Created License model

--- FOSSDB_web/apps/fossdb/models.py
+++ FOSSDB_web/apps/fossdb/models.py
@@ -2,10 +2,21 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class License(models.Model):
+    short_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.short_name
+
+
 class Project(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
+    license = models.ManyToManyField(License)
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
