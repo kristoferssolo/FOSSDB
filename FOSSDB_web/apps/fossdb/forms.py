5d5944fa49aee48d6516544d744afec4df40e093
Kristofers Solo <kristians.cagulis@proton.me>
2023-04-07 14:32:08 +0300

    Created License form

--- FOSSDB_web/apps/fossdb/forms.py
+++ FOSSDB_web/apps/fossdb/forms.py
@@ -1,9 +1,29 @@
-from django import forms
+from django.forms import (CheckboxSelectMultiple, ModelForm,
+                          ModelMultipleChoiceField, TextInput)
 
-from .models import Project
+from .models import License, Project
 
 
-class ProjectForm(forms.ModelForm):
+class ProjectForm(ModelForm):
+
     class Meta:
         model = Project
-        fields = ["title", "description"]
+        fields = ["title", "description", "license"]
+
+        widgets = {
+            "title": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Project name",
+            }),
+            "description": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Description",
+            }),
+        }
+    license = ModelMultipleChoiceField(queryset=License.objects.all(), widget=CheckboxSelectMultiple)
+
+
+class LicenseForm(ModelForm):
+    class Meta:
+        model = License
+        fields = ["short_name", "full_name", "url", "description"]
