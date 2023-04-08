816c430f29cc9a482e05101dc8ccc09d3d9a9be7
Kristofers Solo <kristians.cagulis@proton.me>
2023-04-08 13:50:45 +0300

    Created HostingPlatform app

---
+++ FOSSDB_web/apps/hosting_platform/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HostingPlatformConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hosting_platform'
--- FOSSDB_web/settings.py
+++ FOSSDB_web/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = config["ALLOWED_HOSTS"]
 INSTALLED_APPS = [
     "fossdb",
     "account",
+    "hosting_platform",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
