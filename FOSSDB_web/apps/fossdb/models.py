e74853d89f4900a6b7f2f4fd1c62694e5ba6c3c8
Kristofers Solo <kristians.cagulis@proton.me>
2023-04-09 13:36:41 +0300

    Created OS model

--- FOSSDB_web/apps/fossdb/models.py
+++ FOSSDB_web/apps/fossdb/models.py
@@ -3,6 +3,12 @@ import uuid
 from django.conf import settings
 from django.db import models
 
+from .host.models import HostingPlatform
+from .language.models import ProgrammingLanguage
+from .license.models import License
+from .operating_system.models import OperatingSystem
+from .tag.models import Tag
+
 User = settings.AUTH_USER_MODEL
 
 
@@ -11,10 +17,11 @@ class Project(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
-    licenses = models.ManyToManyField("License")
-    programming_languages = models.ManyToManyField("ProgrammingLanguage", through="ProjectProgrammingLanguage", related_name="projects")
-    hosting_platform = models.ManyToManyField("HostingPlatform", through="ProjectHostingPlatform", related_name="projects")
-    tag = models.ManyToManyField("Tag")
+    licenses = models.ManyToManyField(License)
+    programming_languages = models.ManyToManyField(ProgrammingLanguage, through="ProjectProgrammingLanguage", related_name="projects")
+    hosting_platform = models.ManyToManyField(HostingPlatform, through="ProjectHostingPlatform", related_name="projects")
+    tag = models.ManyToManyField(Tag)
+    os = models.ManyToManyField(OperatingSystem)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
