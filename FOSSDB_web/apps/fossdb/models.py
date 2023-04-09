60f252321bad3ee729cbfdf30caa6449f4b0d2f5
Kristofers Solo <kristians.cagulis@proton.me>
2023-04-09 14:06:15 +0300

    Created Star model

--- FOSSDB_web/apps/fossdb/models.py
+++ FOSSDB_web/apps/fossdb/models.py
@@ -7,6 +7,7 @@ from .host.models import HostingPlatform
 from .language.models import ProgrammingLanguage
 from .license.models import License
 from .operating_system.models import OperatingSystem
+from .star.models import Star
 from .tag.models import Tag
 
 User = settings.AUTH_USER_MODEL
@@ -14,7 +15,7 @@ User = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
     licenses = models.ManyToManyField(License)
@@ -22,12 +23,20 @@ class Project(models.Model):
     hosting_platform = models.ManyToManyField(HostingPlatform, through="ProjectHostingPlatform", related_name="projects")
     tag = models.ManyToManyField(Tag)
     os = models.ManyToManyField(OperatingSystem)
+    star = models.ManyToManyField(Star, related_name="projects_star")
     date_created = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        if not self.uuid:
-            self.uuid = uuid.uuid5(uuid.NAMESPACE_URL, f"{self.author.username}-{self.name}")
-        super().save(*args, **kwargs)
+    @property
+    def star_amount(self):
+        return self.star.count()
+
+    def get_absolute_url(self):
+        return f"/{self.author.name}/{self.name}"
 
     def __str__(self):
         return f"{self.author} | {self.name}"
+
+    def save(self, *args, **kwargs):
+        if not self.uuid:
+            self.uuid = uuid.uuid3(uuid.uuid4(), f"{self.author.username}-{self.name}")
+        super().save(*args, **kwargs)
