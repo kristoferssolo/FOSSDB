c6f1092361d2c303cb3433bfe5b227d9c3c2c774
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-25 13:49:39 +0000

    Add `OS` default return option

--- FOSSDB/apps/fossdb/operating_system/models.py
+++ FOSSDB/apps/fossdb/operating_system/models.py
@@ -5,6 +5,8 @@ class OperatingSystem(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, default="")
 
+    def __str__(self):
+        return self.name
 
 
 class OperatingSystemVersion(models.Model):
