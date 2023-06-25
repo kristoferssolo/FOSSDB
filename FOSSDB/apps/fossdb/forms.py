f995959286036a2503601799ec595e27d8fec825
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-25 14:04:18 +0000

    Change `CharFiled` to `URLField`

--- FOSSDB/apps/fossdb/forms.py
+++ FOSSDB/apps/fossdb/forms.py
@@ -1,25 +1,25 @@
 from django import forms
 
-from .hosting_platform.models import HostingPlatform, ProjectHostingPlatform
+from .hosting_platform.models import HostingPlatform
 
 from .models import Project
 
 
 class ProjectForm(forms.ModelForm):
     hosting_platform = forms.ModelChoiceField(queryset=HostingPlatform.objects.all())
-    url = forms.CharField(max_length=100)
+    url = forms.URLField(max_length=100)
 
     class Meta:
         model = Project
         fields = (
             "name",
             "description",
+            "url",
+            "hosting_platform",
             "license",
             "tag",
             "operating_system",
         )
-
-        exclude = ("hosting_platform",)
 
         widgets = {
             "name": forms.TextInput(
