236d169c2cadeb464b562a1c6bc5cbcb400c0510
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-25 13:49:25 +0000

    Move `OS.version` to separate model

--- FOSSDB/apps/fossdb/operating_system/models.py
+++ FOSSDB/apps/fossdb/operating_system/models.py
@@ -4,4 +4,14 @@ from django.db import models
 class OperatingSystem(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, default="")
-    version = models.CharField(max_length=50, blank=True)
+
+
+
+class OperatingSystemVersion(models.Model):
+    operating_system = models.ForeignKey(OperatingSystem, on_delete=models.CASCADE)
+    version = models.CharField(max_length=50, blank=True, default="")
+    codename = models.CharField(max_length=100, blank=True, default="")
+    is_lts = models.BooleanField(blank=True, default=False)
+
+    def __str__(self):
+        return f"{self.operating_system.name} {self.version} {'LTS' if self.is_lts else ''}"
