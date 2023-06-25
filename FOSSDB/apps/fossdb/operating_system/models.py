5061d25c80475898add7d56c18ec77c04cb08374
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-25 13:48:56 +0000

    Make `OS.name` field unique

--- FOSSDB/apps/fossdb/operating_system/models.py
+++ FOSSDB/apps/fossdb/operating_system/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class OperatingSystem(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, default="")
     version = models.CharField(max_length=50, blank=True)
