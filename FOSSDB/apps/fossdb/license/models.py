37aa41985512e08eb221fad95a54d837998674d4
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-25 13:48:04 +0000

    Make `License.short_name` and `License.full_name` unique

--- FOSSDB/apps/fossdb/license/models.py
+++ FOSSDB/apps/fossdb/license/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class License(models.Model):
-    short_name = models.CharField(max_length=50)
-    full_name = models.CharField(max_length=100, blank=True, default="")
+    short_name = models.CharField(max_length=50, unique=True)
+    full_name = models.CharField(max_length=100, unique=True)
     url = models.URLField(blank=True, default="")
-    description = models.TextField(blank=True, default="")
+    text = models.TextField(blank=True, default="")
 
     def __str__(self):
         return self.short_name
