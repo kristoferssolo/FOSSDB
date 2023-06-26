927e46dfee72c9603313cad36c04ae5d3c63020b
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-26 16:50:20 +0000

    Removed uuid gen

--- FOSSDB/apps/fossdb/models.py
+++ FOSSDB/apps/fossdb/models.py
@@ -12,7 +12,7 @@ User = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
     license = models.ManyToManyField(License, blank=True)
@@ -29,8 +29,3 @@ class Project(models.Model):
 
     def __str__(self):
         return f"{self.owner} | {self.name}"
-
-    def save(self, *args, **kwargs):
-        if not self.uuid:
-            self.uuid = uuid.uuid3(uuid.uuid4(), f"{self.owner.username}-{self.name}")
-        super().save(*args, **kwargs)
