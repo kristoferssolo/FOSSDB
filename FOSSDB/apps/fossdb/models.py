558a52d319daa9c9db6ff2fab06032ef5559585c
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-26 14:07:14 +0000

    Renamed `Project.author` to `Project.owner`

--- FOSSDB/apps/fossdb/models.py
+++ FOSSDB/apps/fossdb/models.py
@@ -12,7 +12,7 @@ User = settings.AUTH_USER_MODEL
 
 class Project(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
     license = models.ManyToManyField(License, blank=True)
@@ -26,12 +26,12 @@ class Project(models.Model):
 
     @property
     def absolute_url(self):
-        return f"/{self.author.name}/{self.name}"
+        return f"/{self.owner}/{self.name}"
 
     def __str__(self):
-        return f"{self.author} | {self.name}"
+        return f"{self.owner} | {self.name}"
 
     def save(self, *args, **kwargs):
         if not self.uuid:
-            self.uuid = uuid.uuid3(uuid.uuid4(), f"{self.author.username}-{self.name}")
+            self.uuid = uuid.uuid3(uuid.uuid4(), f"{self.owner.username}-{self.name}")
         super().save(*args, **kwargs)
