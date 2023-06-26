2528d7d7ad39f867eb6b42c0d395af5ea6e479b6
Kristofers Solo <kristians.cagulis@pm.me>
2023-06-26 15:00:08 +0000

    Added `Project` create view

--- FOSSDB/apps/fossdb/urls.py
+++ FOSSDB/apps/fossdb/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add/", views.add_project, name="add-project"),
+    path("add/", views.ProjectCreateView.as_view(), name="add-project"),
     path("<str:username>/<str:project_name>/", views.ProjectDetailView.as_view(), name="project-detail"),
     path("<str:username>/<str:project_name>/update/", views.ProjectUpdateView.as_view(), name="project-update"),
     path("<str:username>/<str:project_name>/delete/", views.ProjectDeleteView.as_view(), name="project-delete"),
--- FOSSDB/apps/fossdb/views.py
+++ FOSSDB/apps/fossdb/views.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.conf import settings
-from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
@@ -14,47 +13,45 @@ from .programming_language.forms import ProgrammingLanguageForm
 User = settings.AUTH_USER_MODEL
 
 
-@login_required(login_url="login/")
-@permission_required("fossdb.add_project", login_url="login/", raise_exception=True)
-def add_project(request):
-    project_form = ProjectForm(request.POST or None)
-    hosting_platform_form = HostingPlatformForm(request.POST or None)
-    programming_language_form = ProgrammingLanguageForm(request.POST or None)
-
-    _forms: dict[str, forms.ModelForm] = {
-        "project_form": project_form,
-        "hosting_platform_form": hosting_platform_form,
-        "programming_language_form": programming_language_form,
-    }
-
-    if request.method == "POST":
-        if all([form.is_valid() for form in _forms.values()]):
-            project = project_form.save(commit=False)
-            project.author = request.user
-            project.save()
-
-            hosting_platform = hosting_platform_form.save(commit=False)
-            hosting_platform.project = project
-            hosting_platform.save()
-
-            # TODO: allow adding multiple languages
-            programming_language = programming_language_form.save(commit=False)
-            programming_language.project = project
-            programming_language.save()
-
-            project.save_m2m()
-            return redirect("index")
-
-    context = {"title": "Add project", **_forms}
-    return render(request, "fossdb/add_project.html", context)
-
-
 def index(request):
     context = {
         "title": "FOSSDB",
         "projects": Project.objects.all(),
     }
     return render(request, "fossdb/index.html", context)
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = "fossdb/add_project.html"
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        response = super().form_valid(form)
+
+        hosting_platform_form = HostingPlatformForm(self.request.POST, instance=self.object)
+        if hosting_platform_form.is_valid():
+            hosting_platform = hosting_platform_form.save(commit=False)
+            hosting_platform.project = self.object
+            hosting_platform.save()
+
+        # TODO: allow adding multiple languages
+        programming_language_form = ProgrammingLanguageForm(self.request.POST, instance=self.object)
+        if programming_language_form.is_valid():
+            programming_language = programming_language_form.save(commit=False)
+            programming_language.project = self.object
+            programming_language.save()
+
+        return response
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Add project"
+        context["project_form"] = ProjectForm()
+        context["hosting_platform_form"] = HostingPlatformForm()
+        context["programming_language_form"] = ProgrammingLanguageForm()
+        return context
 
 
 class ProjectDetailView(DetailView):
